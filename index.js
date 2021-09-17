bb9b8e10e9fb2f89207b67e959f331e81684773e
agrawlins <agrawlins@gmail.com>
2021-09-16 22:46:29 -0400

    Created db.js to handle MongoDB connection, and default.json in a gitignore folder (config) to hold the connection data

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const mongoose = require('mongoose');
+const connectDB = require('./backend/startup/db');
 
-mongoose
-    .connect('mongodb+srv://MerrimanLyon:13qeadzc%21%23QEADZC@cluster0.xqosq.mongodb.net/kisadb?retryWrites=true&w=majority',
-    {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch((err) => console.log(`Could not connect to MongoDB. ERROR: ${err}`));
+connectDB();
