dc0e272a460d3f0d85185209126836c6939d9510
agrawlins <agrawlins@gmail.com>
2021-09-16 22:53:44 -0400

    Created product.js per the Node_Express_MongoDB Tutorial (will change this out later)

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -7,7 +7,7 @@ function connectDB() {
         {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('Connected to MongoDB...'))
     .catch((err) => console.log(`Could not connect to MongoDB. ERROR: ${err}`));
-    process.exit(1);
+    
 }
 
 module.exports = connectDB;
