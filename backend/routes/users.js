9356c31074a2ec90464e5435504a9a024e52b096
agrawlins <agrawlins@gmail.com>
2021-09-19 22:39:16 -0400

    added DELETE functionality to users.js. Can now delete users & shopping cart items

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -90,11 +90,37 @@ router.put('/:id', async (req, res) => {
     }
 });
 
+//UPDATES PRODUCT INFO IN USER'S SHOPPING CART
+router.put('/:userId/shoppingCart/:productId', async (req, res) => {
+    try{
+        const{error}=validate(req.body);
+        if (error)
+            return res.status(400).send(error);
 
+        const user = await User.findById(req.params.userId);
+        if(!user)
+            return restart.status(400).send(`The user with id "${req.params.userId}" does not exist.`);
+            
+        const product = await user.shoppingCart.id(req.params.productId);
+        if(!product)
+            return res.status(400).send(`The product with id "${req.params.id}" does not exist.`);
+                product.name = req.body.name,
+                product.description = req.body.description,
+                product.category = req.body.category,
+                product.price = req.body.price,
+                product.dateModified = Date.now(),
+
+        await user.save();
+        return res.send(product);
+    }catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+//DELETES A USER ACCOUNT
 router.delete('/:id', async (req, res) => {
     try{
         const user = await User.findByIdAndRemove(req.params.id);
-
         if(!user)
             return res.status(400).send(`The user with id "${req.params.id}" does not exist`);
         return res.send(user);
@@ -102,5 +128,26 @@ router.delete('/:id', async (req, res) => {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });
+
+//DELETES A PRODUCT IN USER'S SHOPPING CART
+router.delete('/:userId/shoppingCart/:productId', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.userId);
+        if(!user)
+            return res.status(400).send(`The user with id "${req.params.id}" does not exist`);
+
+        const product = await user.shoppingCart.id(req.params.productId);
+        if(!product)
+            return res.status(400).send(`The product with id "${req.params.id}" does not exist.`);
+        
+        product = await product.remove();
+
+        await user.save();
+        return res.send(user);
+    }catch (ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
 
 module.exports = router;
